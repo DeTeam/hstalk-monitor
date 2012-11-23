f8cdc6c4b1ec88a01f9dd243b05e613c64f12180
DeTeam <timur.deteam@gmail.com>
2012-11-23 11:59:50 +0400

    Subscription -> JSON cacheing

---
+++ server/Beaninfo/Stalker/Cacher.hs
@@ -0,0 +1,39 @@
+module Beaninfo.Stalker.Cacher (
+    retrieve,
+    cache,
+    newCacher,
+    Cacher()
+  ) where
+
+
+import Data.ByteString.Lazy (ByteString)
+import qualified Data.Map as M
+import Data.Maybe
+
+import Control.Applicative ( (<$>) )
+import Control.Concurrent.MVar ( MVar, newMVar, modifyMVar_, readMVar )
+
+import Beaninfo.Types
+
+--
+-- Возможно, тут понядобится трансформер на Reader/State
+--
+
+
+type Cacher = MVar (M.Map String ByteString)
+
+newCacher :: IO Cacher
+newCacher = newMVar M.empty
+
+getKey :: ClientSubscription -> String
+getKey = show
+
+retrieve :: Cacher -> ClientSubscription -> IO (Maybe ByteString)
+retrieve cacher sub = M.lookup (getKey sub) <$> readMVar cacher
+
+cache :: Cacher -> ClientSubscription -> IO ByteString -> IO ByteString
+cache cacher sub action = do
+  value <- action
+  let updateHash hash = M.insert (getKey sub) value hash
+  modifyMVar_ cacher (return . updateHash)
+  return value
--- server/Beaninfo/Stalker/Server.hs
+++ server/Beaninfo/Stalker/Server.hs
@@ -15,15 +15,17 @@ import Network.Beanstalk (BeanstalkServer)
 import qualified Network.Beanstalk as BS
 import qualified Data.Aeson as JSON
 import Data.Map (Map, insert, fromList, toList)
+import Data.Maybe
 
 import Beaninfo.Types
-
+import Beaninfo.Stalker.Cacher
 
 loopNotification :: BeanstalkServer -> BFunction -> IO ()
 loopNotification server broadcast = do
     putStrLn "looping"
     tubes <- BS.listTubes server
-    broadcast $ getUserMessage server
+    cacher <- newCacher
+    broadcast $ getUserMessage server cacher
     threadDelay t
     loopNotification server broadcast
   where t = 5000000
@@ -38,14 +40,20 @@ muteMap :: Map S.ByteString S.ByteString -> Map S.ByteString BasicServerInfo
 muteMap m = fromList . map f $ toList m
   where f (k,v) = (k, CommonServerInfo v)
 
-getUserMessage :: BeanstalkServer -> Client -> IO ByteString
-getUserMessage server c = do
-  putStrLn .show $ getClientSubscription c
-  liftA JSON.encode $
-    case getClientSubscription c of 
-      GeneralInfo -> do
-        hash <- liftA muteMap $ BS.statsServer server
-        tubes <- BS.listTubes server
-        return $ insert "tubes" (ListServerInfo tubes) hash
-      TubeInfo tube -> liftA muteMap $ BS.statsTube server tube
-      JobInfo job -> liftA muteMap $ BS.statsJob server job
+getUserMessage :: BeanstalkServer -> Cacher -> Client -> IO ByteString
+getUserMessage server cacher c = do
+  putStrLn . show $ sub
+  fromCache <- retrieve cacher sub
+  case fromCache of
+    Just v -> return v
+    _ -> cache cacher sub $
+        liftA JSON.encode $ do
+          putStrLn "Actually go for data"
+          case sub of 
+            GeneralInfo -> do
+              hash <- liftA muteMap $ BS.statsServer server
+              tubes <- BS.listTubes server
+              return $ insert "tubes" (ListServerInfo tubes) hash
+            TubeInfo tube -> liftA muteMap $ BS.statsTube server tube
+            JobInfo job -> liftA muteMap $ BS.statsJob server job
+  where sub = getClientSubscription c
